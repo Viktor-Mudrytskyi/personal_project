ed5eee974901ce77f0c30740440edcdb0d7fa3a6
painedViktor <mudritskiy.vk@gmail.com>
2023-07-05 12:55:23 +0300

    hotfix

--- lib/features/presentation/basic_widgets/app_snackbar.dart
+++ lib/features/presentation/basic_widgets/app_snackbar.dart
@@ -73,6 +73,7 @@ class CustomSnackBarState extends State<CustomSnackBar>
             // height: 60,
             margin: EdgeInsets.symmetric(
                 horizontal: 18, vertical: (widget.screenSize.height - 50) / 2),
+            padding: const EdgeInsets.symmetric(horizontal: 20),
             decoration: BoxDecoration(
                 borderRadius: BorderRadius.circular(30),
                 color: appTheme.appColors.authButtonFill,
